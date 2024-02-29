1fc8c1b57fdfc878b7e050abbc3efc3e52316153
HoggScripts <143638417+HoggScripts@users.noreply.github.com>
2024-02-29 16:50:14 +0000

    Migrated identity tables to database
    Migrated identity tables to database

--- eCommerceRESTful/eCommerceRESTful/Migrations/20240229164827_IdentityAdded.Designer.cs
+++ eCommerceRESTful/eCommerceRESTful/Migrations/20240229164827_IdentityAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eCommerceRESTful.Models;
 
@@ -11,9 +12,11 @@ using eCommerceRESTful.Models;
 namespace eCommerceRESTful.Migrations
 {
     [DbContext(typeof(eCommerceContext))]
-    partial class eCommerceContextModelSnapshot : ModelSnapshot
+    [Migration("20240229164827_IdentityAdded")]
+    partial class IdentityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- eCommerceRESTful/eCommerceRESTful/obj/Debug/net8.0/eCommerceRESTful.AssemblyInfo.cs
+++ eCommerceRESTful/eCommerceRESTful/obj/Debug/net8.0/eCommerceRESTful.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerceRESTful")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6c8bca0279414ec21ec3dc96c88048f5603de8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3741d3c28b41fe7093f531730259cc567451e52e")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerceRESTful")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerceRESTful")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
