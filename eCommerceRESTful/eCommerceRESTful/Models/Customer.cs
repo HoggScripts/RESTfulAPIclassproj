f061d3e61928fa004e6757e2086f472239068b20
HoggScripts <143638417+HoggScripts@users.noreply.github.com>
2024-03-03 13:50:32 +0000

    Updated customer model
    Removed password requirement.

--- eCommerceRESTful/eCommerceRESTful/Models/Customer.cs
+++ eCommerceRESTful/eCommerceRESTful/Models/Customer.cs
@@ -8,10 +8,7 @@ public class Customer
     public int CustomerId { get; set; } // PK
     public string Name { get; set; }
     public string Email { get; set; }
-    
-    [JsonIgnore]
-    public string Password { get; set; }
-    
+
     [JsonIgnore]
     public ICollection<Order>? Orders { get; set; }
     
